f27edc96004891e3e29bf019fbf3a92f92e6553f
Thomas S <sellier.thomas@hotmail.fr>
2018-10-26 00:57:36 +0200

    ajout du début du script qui traite le json en spectre audio

---
+++ javascript/testJSON.php
@@ -0,0 +1,17 @@
+<?php
+
+
+echo('<!DOCTYPE html>');
+echo('<html lang="fr">');
+echo('<head>');
+echo('<meta charset="UTF-8">');
+echo('<title>Title</title>');
+echo('<link rel="stylesheet" type="text/css" href="style.css">');
+echo('</head>');
+echo('<body>');
+echo('<canvas id="canvas"></canvas>');
+echo('<script src="traitementJSON.js"></script>');
+echo('</body>');
+echo('</html>');
+
+?>
