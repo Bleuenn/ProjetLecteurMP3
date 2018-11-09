cc067fa52a1e10b9490d25d757a3f691564f13a9
Thomas S <sellier.thomas@hotmail.fr>
2018-11-09 11:51:36 +0100

    commit

--- javascript/testJSON.php
+++ javascript/testJSON.php
@@ -1,17 +1,3 @@
 <?php
 
-
-echo('<!DOCTYPE html>');
-echo('<html lang="fr">');
-echo('<head>');
-echo('<meta charset="UTF-8">');
-echo('<title>Title</title>');
-echo('<link rel="stylesheet" type="text/css" href="style.css">');
-echo('</head>');
-echo('<body>');
-echo('<canvas id="canvas"></canvas>');
-echo('<script src="traitementJSON.js"></script>');
-echo('</body>');
-echo('</html>');
-
-?>
+include 'index.html';
