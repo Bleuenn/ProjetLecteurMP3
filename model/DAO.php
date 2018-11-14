1f8d1d7a92672f9ccefcb4301f83db12ff674eb8
Thomas S <sellier.thomas@hotmail.fr>
2018-11-14 16:57:21 +0100

    la connexion à la base de donnée se fait

--- model/DAO.php
+++ model/DAO.php
@@ -3,27 +3,42 @@
  * Classe d'accées aux données mongodb
  */
 
-phpinfo();
+require '../vendor/autoload.php';
 
-$dbhost = "localhost";
+$dbhost = "localhost:27017";
 $dbname = "morceau";
 
 try {
+    $manager = new MongoDB\Driver\Manager("mongodb://$dbhost");
 
-    $mongo = new Mongo("mongodb://$dbhost");
-} catch (MongoConnectionException $e) {
+    $stats = new MongoDB\Driver\Command(["dbstats" => 1]);
+    $res = $manager->executeCommand("morceau", $stats);
 
+    $stats = current($res->toArray());
+
+    print_r($stats);
+
+} catch (Mongo\Driver\Exception\Exception $e) {
     echo $e->getMessage();
-}
 
-$db = $mongo->$dbname;
+    $filename = basename(__FILE__);
+
+    echo "The $filename script has experienced an error.\n";
+    echo "It failed with the following exception:\n";
+
+    echo "Exception:", $e->getMessage(), "\n";
+    echo "In file:", $e->getFile(), "\n";
+    echo "On line:", $e->getLine(), "\n";
+}
+/*
+$db = $manager->$dbname;
 
 var_dump($db);
 
-$collection = $db->shows;
+$collection = $db->show();
 
 $cursor = $collection->find();
 
 foreach($cursor as $document){
     var_dump($document);
-}
+}*/
