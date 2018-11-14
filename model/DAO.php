6fa3309bdab41729803556be31c03ac0c4ecb3eb
M-Leveque <leveque.melvin@gmail.com>
2018-11-14 14:58:10 +0100

    test mongodb

--- model/DAO.php
+++ model/DAO.php
@@ -3,3 +3,27 @@
  * Classe d'accées aux données mongodb
  */
 
+phpinfo();
+
+$dbhost = "localhost";
+$dbname = "morceau";
+
+try {
+
+    $mongo = new Mongo("mongodb://$dbhost");
+} catch (MongoConnectionException $e) {
+
+    echo $e->getMessage();
+}
+
+$db = $mongo->$dbname;
+
+var_dump($db);
+
+$collection = $db->shows;
+
+$cursor = $collection->find();
+
+foreach($cursor as $document){
+    var_dump($document);
+}
