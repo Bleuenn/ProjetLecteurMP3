37eb4bd543db8f4790af90799031f1bda8ba092f
M-Leveque <leveque.melvin@gmail.com>
2018-11-16 17:08:13 +0100

    HTML de la maquette du lecteurMp3

---
+++ view/maquette.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leveque_m
+ * Date: 16/11/18
+ * Time: 16:32
+ */
+
+?>
+
+<div id="lecteur">
+    <img src="pochette.png" id="pochette_album"/>
+    <div id="infos_musique">
+        <div>
+            <div>
+                <h2 id="nom_artiste">nom artiste</h2>
+                <h1 id="nom_musique">Titre musique</h1>
+            </div>
+           <div>
+                <span id="btn_like">333K</span>
+                <span id="btn_partage">partager</span>
+           </div>
+        </div>
+        <div>
+            <div id="weaveform"><!-- Weaveform --></div>
+        </div>
+        <div>
+            <div>
+                <div id="time_timer">1:31</div>
+                <div id="time_total">3:11</div>
+            </div>
+            <div>
+                <span id="btn_previous"></span>
+                <span id="btn_play"></span>
+                <span id="btn_next"></span>
+                <span id="btn_son"></span>
+            </div>
+            <div>
+                <span id="nb_play">16.5M</span>
+                <span id="nb_comm">2.280</span>
+            </div>
+        </div>
+    </div>
+</div>
