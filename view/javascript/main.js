fecb9f2db4a006dd6a19e7ec66e3e07c3686973e
Thomas <sellier.thomas@hotmail.fr>
2019-01-18 15:25:40 +0100

    ajout d'un main.js

---
+++ view/javascript/main.js
@@ -0,0 +1,43 @@
+let morceau = new Morceau("Doing Yoga", "Kazy Lambist"),
+ 	lecteur = new Lecteur(),
+	data = morceau.getData();
+
+lecteur.setCurrentMorceau(morceau);
+morceau.setValuesWaveform(data);
+// morceau.getValuesWaveform(this.currentMorceau.getValuesWaveform())
+console.log("data: "+ data);
+console.log("nblike: "+lecteur.currentMorceau.getNbLike());
+console.log("values: "+lecteur.currentMorceau.getValuesWaveform());
+
+lecteur.drawSVG(lecteur.currentMorceau.getValuesWaveform());
+window.addEventListener('resize', function () {
+	lecteur.resizeBar();
+});
+
+lecteur.player(lecteur.currentMorceau.getPath());
+
+lecteur.initialisation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
