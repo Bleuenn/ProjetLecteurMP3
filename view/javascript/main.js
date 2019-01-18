fc688c33beb138effb6f8183bbefb078a0a797e5
M-Leveque <leveque.melvin@gmail.com>
2019-01-18 16:08:05 +0100

    - Rédaction des commentaires du fichier admin.ctrl - Réalisation d'un affichage des données en format JSON

--- view/javascript/main.js
+++ view/javascript/main.js
@@ -1,22 +1,39 @@
-let morceau = new Morceau("Doing Yoga", "Kazy Lambist"),
- 	lecteur = new Lecteur(),
-	data = morceau.getData();
+var req = new XMLHttpRequest();
 
-lecteur.setCurrentMorceau(morceau);
-morceau.setValuesWaveform(data);
-// morceau.getValuesWaveform(this.currentMorceau.getValuesWaveform()) //
-console.log("data: "+ data);
-console.log("nblike: "+lecteur.currentMorceau.getNbLike());
-console.log("values: "+lecteur.currentMorceau.getValuesWaveform());
+req.open('GET', 'http://localhost/ProjetLecteurMP3/index.php?page=admin&id=5c37b38d214fb37593d7c3af&json=true', true); // true pour asynchrone
 
-lecteur.drawSVG(lecteur.currentMorceau.getValuesWaveform());
-window.addEventListener('resize', function () {
-	lecteur.resizeBar();
-});
+req.onreadystatechange = function (e) {
+    if (req.readyState == 4) {
+        if (req.status == 200){
 
-lecteur.player(lecteur.currentMorceau.getPath());
+            let json = JSON.parse(req.responseText);
+            console.log(json);
+            console.log(json._id.$oid);
+            let morceau = new Morceau(json._id.$oid, json.titre, json.album, json.artiste, json.cover, 0, 0, json.duree, 0, 0, json.listePoint, json.cheminMP3),
+                lecteur = new Lecteur();
 
-lecteur.initialisation();
+            lecteur.setCurrentMorceau(morceau);
+            morceau.setValuesWaveform( lecteur.currentMorceau.getValuesWaveform() );
+            // morceau.getValuesWaveform(this.currentMorceau.getValuesWaveform()) //
+            console.log("data: "+ lecteur.currentMorceau.getValuesWaveform() );
+            console.log("nblike: "+lecteur.currentMorceau.getNbLike());
+            console.log("values: "+lecteur.currentMorceau.getValuesWaveform());
+
+            lecteur.drawSVG(lecteur.currentMorceau.getValuesWaveform());
+            window.addEventListener('resize', function () {
+                lecteur.resizeBar();
+            });
+
+            lecteur.player(lecteur.currentMorceau.getPath());
+
+            lecteur.initialisation();
+
+        }
+        else console.log("Erreur pendant le chargement de la page.\n");
+    }
+};
+
+req.send(null);
 
 
 
